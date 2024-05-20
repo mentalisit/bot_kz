6e86d03dee20a10726f01c0d87a161e190b08a5a
mentalisit <mentalisit@gmail.com>
2024-05-20 14:12:19 -0500

    delete garbage

--- bot/app/clients/TelegramClient/helpers.go
+++ bot/app/clients/TelegramClient/helpers.go
@@ -6,34 +6,6 @@ import (
 	"strings"
 )
 
-func (t *Telegram) nameOrNick(UserName, FirstName string) (name string) {
-	if UserName != "" {
-		name = UserName
-
-	} else {
-		name = FirstName
-	}
-	return name
-}
-
-//func (t *Telegram) GetAvatar(userid int64, name string) string {
-//	AvatarTG := "https://thumb.cloud.mail.ru/weblink/thumb/xw1/VLES/v7tqy1nXQ/telegram.png"
-//	userProfilePhotos, err := t.t.GetUserProfilePhotos(tgbotapi.UserProfilePhotosConfig{UserID: userid})
-//	if err != nil || len(userProfilePhotos.Photos) == 0 {
-//		AvatarTG = fmt.Sprintf("https://via.placeholder.com/128x128.png/%s/FFFFFF/?text=%s",
-//			utils.GetRandomColor(), utils.ExtractUppercase(name))
-//		return AvatarTG
-//	}
-//
-//	fileconfig := tgbotapi.FileConfig{FileID: userProfilePhotos.Photos[0][0].FileID}
-//	file, err := t.t.GetFile(fileconfig)
-//	if err != nil {
-//		t.log.ErrorErr(err)
-//		return AvatarTG
-//	}
-//	return "https://api.telegram.org/file/bot" + t.t.Token + "/" + file.FilePath
-//}
-
 func (t *Telegram) getAvatarIsExist(userid int64) string {
 
 	userProfilePhotos, err := t.t.GetUserProfilePhotos(tgbotapi.UserProfilePhotosConfig{UserID: userid})
